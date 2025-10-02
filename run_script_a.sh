472d8535cf369d42f99dd92c29098ffa6e8178b5
Lily Grace Sheram <lilygrace576@gmail.com>
2025-10-02 16:10:32 -0400

    Add files via upload

---
+++ run_script_a.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# run_script.sh
+
+apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/python3_10.sif python3 /mnt/DataAnalysis/AncillaryData/file_database/userUpdateDatabase.py
