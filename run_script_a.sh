69cd5db7723f593994c9e24206bd99367518703e
Lilygrace <lilygrace576@gmail.com>
2025-10-02 17:39:45 -0400

    Update run_script_a.sh
    update filepath

--- run_script_a.sh
+++ run_script_a.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # run_script.sh
 
-apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/python3_10.sif python3 /mnt/DataAnalysis/AncillaryData/file_database/userUpdateDatabase.py
+apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/python3_10.sif python3 /mnt/DataAnalysis/AncillaryData/file_database/UpdateDB/userUpdateDatabase.py
