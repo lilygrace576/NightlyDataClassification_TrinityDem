517c062ecb490a3644336bb7418ee1729d3e9eeb
Lilygrace <lilygrace576@gmail.com>
2025-10-03 15:02:01 -0400

    Update run_script_cpp.sh
    include path argument and change to run executable not source file

--- run_script_cpp.sh
+++ run_script_cpp.sh
@@ -2,5 +2,7 @@
 # run_script.sh
 
 DATE=$1
+P=$2
 echo "Running for $DATE"
-apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/AncillaryData/file_database/NightlyClassificationScripts/NightlyClassification.cpp $DATE y
+echo "Using path: $P"
+apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/AncillaryData/file_database/NightlyClassificationScripts/ClassifyData $DATE $P
